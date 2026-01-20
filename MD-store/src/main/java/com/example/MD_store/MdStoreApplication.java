bb0899b09ca99028434ed8a8c812aca76e428011
muhammed sule dabban <muhammedsulai4@gmail.com>
2026-01-20 11:05:13 +0100

    update

--- MD-store/src/main/java/com/example/MD_store/MdStoreApplication.java
+++ MD-store/src/main/java/com/example/MD_store/MdStoreApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MdStoreApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MdStoreApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        System.setProperty("spring.data.mongodb.uri", "mongodb+srv://tylerth852_db_user:748223@ecommerce.pwwkptv.mongodb.net/?appName=ecommerce");
+        SpringApplication.run(MdStoreApplication.class, args);
+    }
 }
--- MD-store/src/main/java/com/example/MD_store/config/JwtAuthenticationFilter.java
+++ MD-store/src/main/java/com/example/MD_store/config/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,11 +18,12 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Configuration
-@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final JwtService jwtService;
-    private final UserDetailsService userDetailsService;
+    @Autowired
+    private  JwtService jwtService;
+    @Autowired
+    private  UserDetailsService userDetailsService;
 
 
     @Override
